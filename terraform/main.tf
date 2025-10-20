48bbb60d94573e440e1666467fb7d965bd77358d
Ajay Vangara <vangaraajay@gmail.com>
2025-10-20 13:50:14 -0400

    Added API Gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,9 +80,11 @@ resource "aws_iam_role_policy_attachment" "lambda_bedrock" {
 resource "aws_lambda_function" "chatbot_lambda" {
   function_name = "Agent-Code"
   runtime       = "python3.11"  # Python runtime
-  handler       = "lambda_function.lambda_handler"
+  handler       = "agent.lambda_handler"
   filename      = "lambda.zip"
   role          = aws_iam_role.lambda_role.arn
+  timeout       = 60
+  memory_size   = 512
 
   environment {
     variables = {
@@ -94,4 +96,78 @@ resource "aws_lambda_function" "chatbot_lambda" {
         Environment = "Compute"
         Project = "AI Powered Planner Chatbot"
     }
+}
+
+//API Gateway
+resource "aws_api_gateway_rest_api" "chatbot_api" {
+  name = "chatbot-api"
+}
+
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.chatbot_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.chatbot_api.execution_arn}/*/*"
+}
+
+//Path to Agent
+resource "aws_api_gateway_resource" "chat_resource" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  parent_id   = aws_api_gateway_rest_api.chatbot_api.root_resource_id
+  path_part   = "chat"
+}
+
+resource "aws_api_gateway_method" "chat_method" {
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id   = aws_api_gateway_resource.chat_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id = aws_api_gateway_resource.chat_resource.id
+  http_method = aws_api_gateway_method.chat_method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.chatbot_lambda.invoke_arn
+}
+
+//Tasks Path (To get the list of tasks)
+resource "aws_api_gateway_resource" "tasks_resource" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  parent_id   = aws_api_gateway_rest_api.chatbot_api.root_resource_id
+  path_part   = "tasks"
+}
+
+resource "aws_api_gateway_method" "tasks_method" {
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id   = aws_api_gateway_resource.tasks_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "tasks_integration" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id = aws_api_gateway_resource.tasks_resource.id
+  http_method = aws_api_gateway_method.tasks_method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.chatbot_lambda.invoke_arn
+}
+
+//API Gateway Deployment
+resource "aws_api_gateway_deployment" "chatbot_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.tasks_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.chatbot_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  stage_name    = "prod"
 }
